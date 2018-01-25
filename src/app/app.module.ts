ee6325d8ba16ac3ba9573201ba36ad9c9b009433
Marko Kajganic <kajganicmarko@gmail.com>
2018-01-25 21:06:32 +0100

    zadatak5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { SearchModule } from './shared/search.module';
 
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './shared/layout/layout.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { MovieRowComponent } from './components/movie-row/movie-row.component';
+import { SearchComponent } from './components/search/search.component';
+
 
 
 @NgModule({
@@ -15,12 +18,14 @@ import { MovieRowComponent } from './components/movie-row/movie-row.component';
     AppComponent,
     LayoutComponent,
     MoviesComponent,
-    MovieRowComponent
+    MovieRowComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    SearchModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
